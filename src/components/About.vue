294a78ba70d97923336ea0cc8e16d5eed2477505
Tsiroto <doufasg@gmail.com>
2020-09-23 23:52:52 +0300

    header animation added

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,7 +4,7 @@
       <v-row>
         <v-spacer></v-spacer>
         <v-col class="relative">
-          <svg id="logo" width="400" height="68" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 68">
+          <svg id="supertitle" width="400" height="68" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 68">
     <path d="M36,17H18.72V29H34.61v9H18.72V57h-11V8H36Z" style="fill:transparent;stroke:#fff;stroke-miterlimit:10;stroke-width:2" />
     <path d="M85.2,57H72.51L64.89,44.42c-.57-1-1.11-1.81-1.64-2.56a13.42,13.42,0,0,0-1.59-1.93A6,6,0,0,0,60,38.7a4.21,4.21,0,0,0-1.9-.43h-3V57h-11V8h17.5Q79.41,8,79.42,21.35a13.71,13.71,0,0,1-.79,4.74A13,13,0,0,1,76.41,30a14.26,14.26,0,0,1-3.47,3,17.86,17.86,0,0,1-4.53,2v.14a6.65,6.65,0,0,1,2.12,1.11,14.3,14.3,0,0,1,2,1.79,23.6,23.6,0,0,1,1.83,2.21c.58.78,1.11,1.55,1.59,2.3ZM55.12,16.29V29.93H59.9a7.93,7.93,0,0,0,5.71-2.05,6.84,6.84,0,0,0,2.19-5.16q0-6.43-7.69-6.43Z" style="fill:transparent;stroke:#fff;stroke-miterlimit:10;stroke-width:2" />
     <path d="M109.8,57.89q-10.51,0-17.15-6.85T86,33.18q0-11.62,6.73-18.8T110.59,7.2q10.5,0,17,6.87T134,32.19q0,11.55-6.72,18.62T109.8,57.89Zm.48-41.19a11.14,11.14,0,0,0-9.23,4.36q-3.4,4.36-3.41,11.54t3.41,11.52a11.81,11.81,0,0,0,18,.11q3.34-4.11,3.35-11.43,0-7.62-3.25-11.86A10.62,10.62,0,0,0,110.28,16.7Z" style="fill:transparent;stroke:#fff;stroke-miterlimit:10;stroke-width:2" />
@@ -38,53 +38,53 @@
   background: rgb(45, 51, 78);
   height: 100vh;
 }
-#logo {
+#supertitle {
   /* position: absolute; */
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
   animation: fill 0.5s ease forwards 3.5s;
 }
-#logo path {
+#supertitle path {
   animation: fill 0.5s ease forwards 3.5s;
 }
 
-#logo path:nth-child(1) {
+#supertitle path:nth-child(1) {
   stroke-dasharray: 187px;
   stroke-dashoffset: 187px;
   animation: line-anim 2s ease forwards;
 }
-#logo path:nth-child(2) {
+#supertitle path:nth-child(2) {
   stroke-dasharray: 255px;
   stroke-dashoffset: 255px;
   animation: line-anim 2s ease forwards 0.3s;
 }
-#logo path:nth-child(3) {
+#supertitle path:nth-child(3) {
   stroke-dasharray: 247px;
   stroke-dashoffset: 247px;
   animation: line-anim 2s ease forwards 0.6s;
 }
-#logo path:nth-child(4) {
+#supertitle path:nth-child(4) {
   stroke-dasharray: 293px;
   stroke-dashoffset: 293px;
   animation: line-anim 2s ease forwards 0.9s;
 }
-#logo path:nth-child(5) {
+#supertitle path:nth-child(5) {
   stroke-dasharray: 176px;
   stroke-dashoffset: 176px;
   animation: line-anim 2s ease forwards 1.2s;
 }
-#logo path:nth-child(6) {
+#supertitle path:nth-child(6) {
   stroke-dasharray: 224px;
   stroke-dashoffset: 224px;
   animation: line-anim 2s ease forwards 1.5s;
 }
-#logo path:nth-child(7) {
+#supertitle path:nth-child(7) {
   stroke-dasharray: 293px;
   stroke-dashoffset: 293px;
   animation: line-anim 2s ease forwards 1.8s;
 }
-#logo path:nth-child(8) {
+#supertitle path:nth-child(8) {
   stroke-dasharray: 256px;
   stroke-dashoffset: 256px;
   animation: line-anim 2s ease forwards 2.1s;
